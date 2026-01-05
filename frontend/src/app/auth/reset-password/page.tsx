ba37c2c6e33ff1bd9688dad2c858708d63cea4b5
Loonyc-c <battulgabathuyg54@gmail.com>
2026-01-05 21:13:56 +0800

    deployment-4

--- frontend/src/app/auth/reset-password/page.tsx
+++ frontend/src/app/auth/reset-password/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Suspense } from 'react'
 import ResetPasswordForm from '@/features/auth/components/ResetPasswordForm'
 import AuthHeader from '@/features/auth/components/AuthHeader'
 
@@ -9,11 +10,19 @@ const ResetPasswordPage = () => {
       <AuthHeader />
       <div className="p-10 flex justify-center items-center h-fit">
         <div className="perspective-[1000px] relative flex flex-col max-w-5xl justify-center items-center w-full">
-          <ResetPasswordForm />
+          <Suspense
+            fallback={
+              <div className="flex justify-center items-center p-8">
+                <div className="text-neutral-600 dark:text-neutral-400">Loading...</div>
+              </div>
+            }
+          >
+            <ResetPasswordForm />
+          </Suspense>
         </div>
       </div>
     </>
   )
 }
 
-export default ResetPasswordPage;
+export default ResetPasswordPage
